775059e5efbee0dea07677203254e3684a39b3f9
GSB0203 <gimseungbin0203@gmail.com>
2024-12-30 09:56:54 +0900

    feat(question): Question
    질문 엔티티를 구현하였습니다.

---
+++ src/main/java/com/sumheart/question/domain/Question.java
@@ -0,0 +1,19 @@
+package com.sumheart.question.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Question {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String content;
+}
