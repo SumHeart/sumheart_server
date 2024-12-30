568ffaf2bda8bf9579ac1e0faab33386dc55bba0
GSB0203 <gimseungbin0203@gmail.com>
2024-12-30 10:19:41 +0900

    feat(question): QuestionRepository
    질문 JpaRepository에서 어노테이션을 제외하여서 추가하였습니다.

--- src/main/java/com/sumheart/question/domain/repository/QuestionRepository.java
+++ src/main/java/com/sumheart/question/domain/repository/QuestionRepository.java
@@ -2,8 +2,8 @@ package com.sumheart.question.domain.repository;
 
 import com.sumheart.question.domain.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
+@Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 }
