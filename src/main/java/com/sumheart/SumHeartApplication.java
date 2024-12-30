d5bffad3fa7c4ee076fde2e771703cd89364938e
GSB0203 <gimseungbin0203@gmail.com>
2024-12-30 09:08:00 +0900

    feat(init): 기본설정

---
+++ src/main/java/com/sumheart/SumHeartApplication.java
@@ -0,0 +1,13 @@
+package com.sumheart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumHeartApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SumHeartApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/sumheart/SumHeartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumheart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumHeartApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
