7dea49b9a182f6e15855703c61df491ae6ce4b52
GSB0203 <gimseungbin0203@gmail.com>
2024-12-30 12:15:32 +0900

    refactor(question): question 경로
    question 폴더를 domain 하위 폴더로 이동하였습니다.

--- src/main/java/com/sumheart/domain/question/domain/Question.java
+++ src/main/java/com/sumheart/domain/question/domain/Question.java
@@ -1,4 +1,4 @@
-package com.sumheart.question.domain;
+package com.sumheart.domain.question.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
--- src/main/java/com/sumheart/domain/question/domain/repository/QuestionRepository.java
+++ src/main/java/com/sumheart/domain/question/domain/repository/QuestionRepository.java
@@ -1,6 +1,6 @@
-package com.sumheart.question.domain.repository;
+package com.sumheart.domain.question.domain.repository;
 
-import com.sumheart.question.domain.Question;
+import com.sumheart.domain.question.domain.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
