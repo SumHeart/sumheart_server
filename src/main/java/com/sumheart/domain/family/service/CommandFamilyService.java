f4f8fe49b11ab747eb9a5538c0be7b040ead671d
GSB0203 <gimseungbin0203@gmail.com>
2025-01-02 00:51:57 +0900

    feat(family): CommandFamilyService
    가족 생성 및 가족 코드 생성 기능을 구현하였습니다.

--- src/main/java/com/sumheart/domain/family/service/CommandFamilyService.java
+++ src/main/java/com/sumheart/domain/family/service/CommandFamilyService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Random;
 
 @Service
 @Transactional
@@ -17,6 +18,11 @@ public class CommandFamilyService {
   private final FamilyRepository familyRepository;
   private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
+  public void create() {
+    Family family = new Family(createInvitationCode());
+    familyRepository.save(family);
+  }
+
   @Scheduled(cron = "0 0 0 * * *")
   public void updateTotalDays() {
     List<Family> families = familyRepository.findAll();
@@ -24,5 +30,17 @@ public class CommandFamilyService {
       family.updateTotalDays();
       familyRepository.save(family);
     }
+  }
+
+  public String createInvitationCode() {
+    Random random = new Random();
+    StringBuilder invitationCode = new StringBuilder(6);
+
+    for (int i = 0; i < 6; i++) {
+      int randomIndex = random.nextInt(CHARACTERS.length());
+      invitationCode.append(CHARACTERS.charAt(randomIndex));
+    }
+
+    return invitationCode.toString();
   }
 }
