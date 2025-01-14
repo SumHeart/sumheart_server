dd0287738fffa16cbb3e17638a902b921c1d80b6
GSB0203 <gimseungbin0203@gmail.com>
2025-01-14 15:52:17 +0900

    feat(oauth): CustomOAuth2User
    CustomOAuth2User를 추가하였습니다.

---
+++ src/main/java/com/sumheart/domain/oauth/service/dto/CustomOAuth2User.java
@@ -0,0 +1,37 @@
+package com.sumheart.domain.oauth.service.dto;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class CustomOAuth2User implements OAuth2User {
+
+    private final UserDto userDto;
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return null;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+
+        return collection;
+    }
+
+    @Override
+    public String getName() {
+        return String.valueOf(userDto.id());
+    }
+
+    public Long getId() {
+        return userDto.id();
+    }
+}
