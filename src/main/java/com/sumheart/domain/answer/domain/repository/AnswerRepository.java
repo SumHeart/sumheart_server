cf4407a9ff34b673c5fc0952aab14f81d1c5d1c5
GSB0203 <gimseungbin0203@gmail.com>
2025-01-15 00:14:02 +0900

    fix(Answer): AnswerRepository
    사용하지 않게된 메서드를 제거하였습니다.

--- src/main/java/com/sumheart/domain/answer/domain/repository/AnswerRepository.java
+++ src/main/java/com/sumheart/domain/answer/domain/repository/AnswerRepository.java
@@ -3,10 +3,7 @@ package com.sumheart.domain.answer.domain.repository;
 import com.sumheart.domain.answer.domain.Answer;
 import com.sumheart.domain.family.domain.Family;
 import com.sumheart.domain.questionRecode.domain.QuestionRecode;
-import com.sumheart.domain.user.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,10 +11,6 @@ import java.util.List;
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
 
-  Answer findByQuestionRecode(QuestionRecode questionRecode);
-  Answer findByQuestionRecodeAndUser(QuestionRecode questionRecode, Users user);
-  @Query("SELECT DISTINCT a FROM Answer a JOIN a.questionRecode qr WHERE a.family = :family")
-  List<Answer> findDistinctByQuestionRecode(@Param("family") Family family);
   List<Answer> findAllByQuestionRecodeAndFamily(QuestionRecode questionRecode, Family family);
   boolean existsByUserIdAndQuestionRecodeId(Long userId, Long questionRecodeId);
 }
