954c395cf9103bdf42dc75cf2a55e8f463f0cfc3
GSB0203 <gimseungbin0203@gmail.com>
2025-01-14 16:38:20 +0900

    feat(family): GetFamilyResponse
    가족 조회를 추가하였습니다.

---
+++ src/main/java/com/sumheart/domain/family/presentation/dto/GetFamilyResponse.java
@@ -0,0 +1,18 @@
+package com.sumheart.domain.family.presentation.dto;
+
+import com.sumheart.domain.family.domain.Family;
+import com.sumheart.domain.user.domain.Users;
+import com.sumheart.domain.user.presentation.dto.UsernameResponse;
+
+import java.util.List;
+
+public record GetFamilyResponse(
+    long totalDay,
+    List<UsernameResponse> families,
+    long myPoint
+) {
+  public static GetFamilyResponse of(Family family, List<Users> users, Users user) {
+    List<UsernameResponse> userNameResponses = users.stream().map(UsernameResponse::from).toList();
+    return new GetFamilyResponse(family.getTotalDays(), userNameResponses, user.getPoint());
+  }
+}
