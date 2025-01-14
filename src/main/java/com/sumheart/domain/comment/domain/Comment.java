fbb78c4de8829fca6b7fab7308d4f87665f70239
GSB0203 <gimseungbin0203@gmail.com>
2025-01-15 01:09:00 +0900

    feat(comment): Comment
    Comment 도메인을 추가하였습니다.

---
+++ src/main/java/com/sumheart/domain/comment/domain/Comment.java
@@ -0,0 +1,42 @@
+package com.sumheart.domain.comment.domain;
+
+import com.sumheart.domain.family.domain.Family;
+import com.sumheart.domain.questionRecode.domain.QuestionRecode;
+import com.sumheart.domain.user.domain.Users;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "questionRecodeId")
+  private QuestionRecode questionRecode;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "familyId")
+  private Family family;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "userId")
+  private Users user;
+
+  @NotNull
+  private String content;
+
+  public Comment(QuestionRecode questionRecode, Family family, Users user, String content) {
+    this.questionRecode = questionRecode;
+    this.family = family;
+    this.user = user;
+    this.content = content;
+  }
+}
