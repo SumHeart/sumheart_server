053361b4c755f64c1c38704ed48012b757e1a370
GSB0203 <gimseungbin0203@gmail.com>
2025-01-14 16:37:01 +0900

    feat(question): QueryQuestionService
    QueryQuestionService를 추가하였습니다.

--- src/main/java/com/sumheart/domain/question/service/QueryQuestionService.java
+++ src/main/java/com/sumheart/domain/question/service/QueryQuestionService.java
@@ -1,8 +1,9 @@
 package com.sumheart.domain.question.service;
 
-import com.sumheart.domain.question.domain.repository.QuestionRepository;
 import com.sumheart.domain.question.domain.Question;
+import com.sumheart.domain.question.domain.repository.QuestionRepository;
 import com.sumheart.domain.question.exception.QuestionNotFoundException;
+import com.sumheart.domain.questionRecode.domain.QuestionRecode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,8 +15,12 @@ public class QueryQuestionService {
 
   private final QuestionRepository questionRepository;
 
-  public Question findById(Long id) {
+  public Question getOne(Long id) {
     return questionRepository.findById(id)
         .orElseThrow(QuestionNotFoundException::new);
+  }
+
+  public Question getNextQuestion(QuestionRecode questionRecode) {
+    return getOne(questionRecode.getQuestion().getId() + 1L);
   }
 }
