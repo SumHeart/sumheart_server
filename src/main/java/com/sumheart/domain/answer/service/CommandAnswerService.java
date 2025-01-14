1e04f5c9c9bd24def9dc6ae7e850e19122d0e102
GSB0203 <gimseungbin0203@gmail.com>
2025-01-14 23:37:17 +0900

    feat(answer): CommandAnswerService
    WeatherSticker를 추가하였습니다.

--- src/main/java/com/sumheart/domain/answer/service/CommandAnswerService.java
+++ src/main/java/com/sumheart/domain/answer/service/CommandAnswerService.java
@@ -2,6 +2,7 @@ package com.sumheart.domain.answer.service;
 
 import com.sumheart.domain.answer.domain.Answer;
 import com.sumheart.domain.answer.domain.repository.AnswerRepository;
+import com.sumheart.domain.answer.domain.value.WeatherSticker;
 import com.sumheart.domain.answer.exception.UserAnswerExistsException;
 import com.sumheart.domain.questionRecode.domain.QuestionRecode;
 import com.sumheart.domain.questionRecode.service.QueryQuestionRecodeService;
@@ -20,13 +21,13 @@ public class CommandAnswerService {
   private final QueryUserService queryUserService;
   private final QueryQuestionRecodeService queryQuestionRecodeService;
 
-  public void create(Long questionRecodeId, String content, Long userId) {
+  public void create(Long questionRecodeId, String content, long weatherStickerCode, Long userId) {
     boolean isDuplicateAnswer = answerRepository.existsByUserIdAndQuestionRecodeId(userId, questionRecodeId);
     if (isDuplicateAnswer) throw new UserAnswerExistsException();
 
     Users user = queryUserService.getOne(userId);
     QuestionRecode question = queryQuestionRecodeService.getOne(questionRecodeId);
-    Answer answer = new Answer(user, user.getFamily(), question, content);
+    Answer answer = new Answer(user, user.getFamily(), question, content, WeatherSticker.fromCode(weatherStickerCode));
     answerRepository.save(answer);
   }
 }
