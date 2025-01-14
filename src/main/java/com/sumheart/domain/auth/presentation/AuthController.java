e24542c281284d1de8193b29d72cc8d4c075c197
GSB0203 <gimseungbin0203@gmail.com>
2025-01-14 15:45:27 +0900

    feat(auth): AuthController
    유저와 관련된 추가 기능을 위한 컨트롤러를 추가하였습니다.

---
+++ src/main/java/com/sumheart/domain/auth/presentation/AuthController.java
@@ -0,0 +1,49 @@
+package com.sumheart.domain.auth.presentation;
+
+import com.sumheart.domain.auth.presentation.dto.AdditionalInfoRequest;
+import com.sumheart.domain.auth.service.implementation.AdditionalInfoUpdater;
+import com.sumheart.domain.auth.service.implementation.ReIssuer;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import static com.sumheart.common.jwt.util.AuthenticationUtil.getMemberId;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final ReIssuer reIssuer;
+    private final AdditionalInfoUpdater additionalInfoUpdater;
+
+    @PostMapping("/reissue")
+    @ResponseStatus(HttpStatus.OK)
+    public void reissue(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) {
+        reIssuer.reissue(request, response);
+    }
+
+    @PostMapping("/users")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateAdditionalInfo(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        @RequestBody AdditionalInfoRequest additionalInfoRequest
+    ) {
+        additionalInfoUpdater.update(request, response, getMemberId(), additionalInfoRequest.username(), additionalInfoRequest.familyDay());
+    }
+
+    @GetMapping("/check")
+    @ResponseStatus(HttpStatus.OK)
+    public void checkAuthStatus() {
+        log.warn("AuthController : /check 성공");
+    }
+
+}
