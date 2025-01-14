776480400ab4c0cf332f0eb1d5910859d8848bcd
GSB0203 <gimseungbin0203@gmail.com>
2025-01-14 16:25:25 +0900

    feat(answer): Answer
    답변을 추가하였습니다.

---
+++ src/main/java/com/sumheart/domain/answer/domain/Answer.java
@@ -0,0 +1,40 @@
+package com.sumheart.domain.answer.domain;
+
+import com.sumheart.domain.family.domain.Family;
+import com.sumheart.domain.questionRecode.domain.QuestionRecode;
+import com.sumheart.domain.user.domain.Users;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Answer {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "userId")
+  private Users user;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "familyId")
+  private Family family;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "questionRecodeId")
+  private QuestionRecode questionRecode;
+
+  private String content;
+
+  public Answer(Users user, Family family, QuestionRecode questionRecode, String content) {
+    this.user = user;
+    this.family = family;
+    this.questionRecode = questionRecode;
+    this.content = content;
+  }
+}
