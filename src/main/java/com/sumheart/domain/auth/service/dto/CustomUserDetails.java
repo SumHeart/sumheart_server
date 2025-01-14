5d807c9ef23085b21ebee3acf08e8d25a3d3963f
GSB0203 <gimseungbin0203@gmail.com>
2025-01-14 15:54:34 +0900

    feat(user): CustomUserDetails
    CustomUserDetails를 추가하였습니다.

---
+++ src/main/java/com/sumheart/domain/auth/service/dto/CustomUserDetails.java
@@ -0,0 +1,56 @@
+package com.sumheart.domain.auth.service.dto;
+
+import com.sumheart.domain.user.domain.Users;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final Users user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+        authorityList.add(() -> "USER");
+
+        return authorityList;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+    public Long getId() { return user.getId(); }
+}
