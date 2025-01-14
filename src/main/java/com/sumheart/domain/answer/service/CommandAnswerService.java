4d490cf13cc63b93852f31ff7576cbc01100ae88
GSB0203 <gimseungbin0203@gmail.com>
2025-01-14 16:28:08 +0900

    feat(answer): CommandAnswerService
    답변 생성 기능을 추가하였습니다.

---
+++ src/main/java/com/sumheart/domain/answer/service/CommandAnswerService.java
@@ -0,0 +1,32 @@
+package com.sumheart.domain.answer.service;
+
+import com.sumheart.domain.answer.domain.Answer;
+import com.sumheart.domain.answer.domain.repository.AnswerRepository;
+import com.sumheart.domain.answer.exception.UserAnswerExistsException;
+import com.sumheart.domain.questionRecode.domain.QuestionRecode;
+import com.sumheart.domain.questionRecode.service.QueryQuestionRecodeService;
+import com.sumheart.domain.user.domain.Users;
+import com.sumheart.domain.user.service.QueryUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CommandAnswerService {
+
+  private final AnswerRepository answerRepository;
+  private final QueryUserService queryUserService;
+  private final QueryQuestionRecodeService queryQuestionRecodeService;
+
+  public void create(Long questionRecodeId, String content, Long userId) {
+    boolean isDuplicateAnswer = answerRepository.existsByUserIdAndQuestionRecodeId(userId, questionRecodeId);
+    if (isDuplicateAnswer) throw new UserAnswerExistsException();
+
+    Users user = queryUserService.getOne(userId);
+    QuestionRecode question = queryQuestionRecodeService.getOne(questionRecodeId);
+    Answer answer = new Answer(user, user.getFamily(), question, content);
+    answerRepository.save(answer);
+  }
+}
