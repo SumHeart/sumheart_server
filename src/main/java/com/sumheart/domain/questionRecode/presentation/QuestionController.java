c7f24488015319633e0f2ede6ea4eda67ca3769e
GSB0203 <gimseungbin0203@gmail.com>
2025-01-14 16:36:13 +0900

    feat(questionRecode): QuestionController
    QuestionController를 추가하였습니다.

---
+++ src/main/java/com/sumheart/domain/questionRecode/presentation/QuestionController.java
@@ -0,0 +1,48 @@
+package com.sumheart.domain.questionRecode.presentation;
+
+import com.sumheart.domain.answer.presentation.dto.AnswerRequest;
+import com.sumheart.domain.answer.presentation.dto.AnswerResponse;
+import com.sumheart.domain.answer.service.CommandAnswerService;
+import com.sumheart.domain.answer.service.QueryAnswerService;
+import com.sumheart.domain.questionRecode.presentation.dto.QuestionResponse;
+import com.sumheart.domain.questionRecode.service.QueryQuestionRecodeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.sumheart.common.jwt.util.AuthenticationUtil.getMemberId;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/questions")
+public class QuestionController {
+
+  private final QueryQuestionRecodeService queryQuestionRecodeService;
+  private final CommandAnswerService commandAnswerService;
+  private final QueryAnswerService queryAnswerService;
+
+  @GetMapping("/{question-id}")
+  public QuestionResponse getQuestion(@PathVariable("question-id") Long questionId) {
+    return QuestionResponse.from(queryQuestionRecodeService.getOne(questionId));
+  }
+
+  @GetMapping
+  public List<QuestionResponse> getQuestions() {
+    return queryQuestionRecodeService.getQuestionRecodes(getMemberId()).stream()
+        .map(QuestionResponse::from)
+        .toList();
+  }
+
+  @GetMapping("/{question-id}/answer")
+  public List<AnswerResponse> getAllAnswer(@PathVariable("question-id") Long questionId) {
+    return queryAnswerService.findAllByQuestionAndFamily(questionId, getMemberId()).stream()
+        .map(AnswerResponse::from)
+        .toList();
+  }
+
+  @PostMapping("/{question-id}/answer")
+  public void createAnswer(@PathVariable("question-id") Long questionId, AnswerRequest answerRequest) {
+    commandAnswerService.create(questionId, answerRequest.content(), getMemberId());
+  }
+}
