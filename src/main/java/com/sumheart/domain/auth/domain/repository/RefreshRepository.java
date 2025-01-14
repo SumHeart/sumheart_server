afc19f2ea46ff36fb702dd4bdf6f594e5c8617c8
GSB0203 <gimseungbin0203@gmail.com>
2025-01-14 16:14:31 +0900

    feat(refresh): RefreshRepository
    RefreshRepository를 추가하였습니다.

---
+++ src/main/java/com/sumheart/domain/auth/domain/repository/RefreshRepository.java
@@ -0,0 +1,15 @@
+package com.sumheart.domain.auth.domain.repository;
+
+import com.sumheart.domain.auth.domain.Refresh;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public interface RefreshRepository extends JpaRepository<Refresh, Long> {
+
+    Boolean existsByRefreshToken(String refreshToken);
+
+    @Transactional
+    void deleteByRefreshToken(String refreshToken);
+}
