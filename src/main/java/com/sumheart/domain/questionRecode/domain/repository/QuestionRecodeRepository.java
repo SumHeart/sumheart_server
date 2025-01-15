c10fad3df136a3b425cbdacef7fb8826b1ceefbb
GSB0203 <gimseungbin0203@gmail.com>
2025-01-15 13:31:36 +0900

    feat(questionRecode): QuestionRecodeRepository
    @Repository를 추가하였습니다.

--- src/main/java/com/sumheart/domain/questionRecode/domain/repository/QuestionRecodeRepository.java
+++ src/main/java/com/sumheart/domain/questionRecode/domain/repository/QuestionRecodeRepository.java
@@ -3,10 +3,12 @@ package com.sumheart.domain.questionRecode.domain.repository;
 import com.sumheart.domain.family.domain.Family;
 import com.sumheart.domain.questionRecode.domain.QuestionRecode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface QuestionRecodeRepository extends JpaRepository<QuestionRecode, Long> {
 
   Optional<QuestionRecode> findTopByFamilyOrderByCreatedAtDesc(Family family);
