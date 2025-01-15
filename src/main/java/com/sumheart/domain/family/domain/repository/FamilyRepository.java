524009556756ad0d413edeea9bb15b1f301d8c21
GSB0203 <gimseungbin0203@gmail.com>
2025-01-15 13:31:17 +0900

    feat(family): FamilyRepository
    @Repository를 추가하였습니다.

--- src/main/java/com/sumheart/domain/family/domain/repository/FamilyRepository.java
+++ src/main/java/com/sumheart/domain/family/domain/repository/FamilyRepository.java
@@ -2,9 +2,11 @@ package com.sumheart.domain.family.domain.repository;
 
 import com.sumheart.domain.family.domain.Family;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface FamilyRepository extends JpaRepository<Family,Long> {
 
   Optional<Family> findIdByInvitationCode(String invitationCode);
