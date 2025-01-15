76aa483650fe70acd6dc9d0482e54f0d70e6ed38
GSB0203 <gimseungbin0203@gmail.com>
2025-01-15 13:33:34 +0900

    refactor(family): GetFamilyResponse
    가독성을 높이게 수정하였습니다.

--- src/main/java/com/sumheart/domain/family/presentation/dto/GetFamilyResponse.java
+++ src/main/java/com/sumheart/domain/family/presentation/dto/GetFamilyResponse.java
@@ -7,12 +7,15 @@ import com.sumheart.domain.user.presentation.dto.UsernameResponse;
 import java.util.List;
 
 public record GetFamilyResponse(
+
     long totalDay,
     List<UsernameResponse> families,
     long myPoint
 ) {
   public static GetFamilyResponse of(Family family, List<Users> users, Users user) {
-    List<UsernameResponse> userNameResponses = users.stream().map(UsernameResponse::from).toList();
+    List<UsernameResponse> userNameResponses = users.stream()
+        .map(UsernameResponse::from)
+        .toList();
     return new GetFamilyResponse(family.getTotalDays(), userNameResponses, user.getPoint());
   }
 }
