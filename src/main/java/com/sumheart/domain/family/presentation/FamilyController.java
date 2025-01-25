a97501ecd21d992a943d2c8b654dc55c2ea891d7
GSB0203 <gimseungbin0203@gmail.com>
2025-01-26 03:05:52 +0900

    feat(Family): FamilyController
    가족 초대 코드 조회를 구현하였습니다.

--- src/main/java/com/sumheart/domain/family/presentation/FamilyController.java
+++ src/main/java/com/sumheart/domain/family/presentation/FamilyController.java
@@ -3,6 +3,7 @@ package com.sumheart.domain.family.presentation;
 import com.sumheart.domain.family.presentation.dto.GetFamilyResponse;
 import com.sumheart.domain.family.presentation.dto.JoinFamilyRequest;
 import com.sumheart.domain.family.service.CommandFamilyService;
+import com.sumheart.domain.family.service.QueryFamilyService;
 import com.sumheart.domain.user.service.CommandUserService;
 import com.sumheart.domain.user.service.QueryUserService;
 import lombok.RequiredArgsConstructor;
@@ -16,13 +17,20 @@ import static com.sumheart.common.jwt.util.AuthenticationUtil.getMemberId;
 @RequiredArgsConstructor
 @RequestMapping("/family")
   public class FamilyController {
+
   private final CommandFamilyService commandFamilyService;
+  private final QueryFamilyService queryFamilyService;
   private final CommandUserService commandUserService;
   private final QueryUserService queryUserService;
 
   @GetMapping
   public GetFamilyResponse getFamily() {
     return queryUserService.getFamily(getMemberId());
+  }
+
+  @GetMapping("/code")
+  public String getFamilyCode() {
+    return queryFamilyService.getCode(getMemberId());
   }
 
   @PostMapping
